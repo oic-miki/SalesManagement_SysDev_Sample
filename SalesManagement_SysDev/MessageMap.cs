dd971bfffd03061fb6b5fd02634b3d2de39d1790
miki@oic.jp <miki@oic.jp>
2022-10-21 18:02:57 +0900

    メッセージオブジェクトの追加。

---
+++ SalesManagement_SysDev/MessageMap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement_SysDev
+{
+    public class MessageMap
+    {
+
+        private Dictionary<String, String> map = new Dictionary<String, String>();
+
+        public String get(String code)
+        {
+
+            String message = map[code];
+            if (message != null)
+            {
+
+                return message;
+
+            }
+
+            return "";
+
+        }
+
+        public String put(String code, String message)
+        {
+
+            map.Add(code, message);
+
+            return message;
+
+        }
+
+    }
+
+}
